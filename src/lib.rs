eeac1a051697d230b48c462b8dc2716e0139c59e
Markus Røyset <maroider@protonmail.com>
2024-03-09 04:23:18 +0100

    It kind of works, though MIRI currently isn't happy with me
    In particular, MIRI doesn't like lines 102 and 104.

--- src/lib.rs
+++ src/lib.rs
@@ -1,256 +1,369 @@
 use std::{
-    cell::Cell,
+    cell::UnsafeCell,
     future::Future,
-    marker::{PhantomData, PhantomPinned},
+    marker::PhantomData,
+    mem,
     pin::Pin,
-    task::Poll,
+    ptr::NonNull,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        mpsc, Arc, Mutex,
+    },
+    task::{Context, Poll, RawWaker, RawWakerVTable, Waker},
 };
 
-pub async fn scope<F, T>(f: F) -> Scope<T, F>
-where
-    F: FnOnce(&mut ScopedSpawner<T>) -> T,
-    T: Send,
-{
-    Scope::<T, F>::new(f)
-}
-
-/// The scope guard.
+/// An executor capable of paralell execution.
 ///
-/// For the scoped tasks to actually be spawned onto the executor, you must await this future, but
-/// you must also yield control all the way back to the [`ScopedSpawnCapableFuture`] at the bottom
-/// of your stack of futures. Once you've done this, your future will only be polled again once all
-/// sub-tasks have finished running.
-pub struct Scope<T, F> {
-    _phantom: PhantomData<T>,
-    f: Option<F>,
-    output: Option<T>,
+/// If [`Executor::spawn`] ends up spawning futures onto a non-multithreaded executor, your program
+/// may deadlock.
+pub trait Executor {
+    type TaskHandle<T>: TaskHandle;
+
+    fn spawn<F, T>(fut: F) -> Self::TaskHandle<T>
+    where
+        F: Future<Output = T> + Send + 'static,
+        T: Send + 'static;
 }
 
-impl<T, F> Scope<T, F> {
-    fn new(f: F) -> Self {
+pub trait TaskHandle {
+    fn abort(&self);
+}
+
+#[must_use = "Must .await the ScopeGuard in order to make progress in subtasks"]
+pub fn scope<'env, 'scope, Ex, F>(f: F) -> ScopeGuard<'scope, Ex>
+where
+    'env: 'scope,
+    Ex: Executor,
+    F: FnOnce(&Scope<'scope, 'env, Ex>),
+{
+    let scope = Scope::new();
+    f(&scope);
+    scope.guard.into_inner().unwrap()
+}
+
+pub struct ScopeGuard<'scope, Ex>
+where
+    Ex: Executor,
+{
+    _phantom: PhantomData<&'scope ()>,
+    tasks_to_spawn: Vec<Box<dyn Future<Output = ()>>>,
+    tasks: Vec<SubTask<Ex::TaskHandle<()>>>,
+    tasks_done: usize,
+    tx: mpsc::Sender<usize>,
+    rx: mpsc::Receiver<usize>,
+}
+
+impl<'scope, Ex> ScopeGuard<'scope, Ex>
+where
+    Ex: Executor,
+{
+    fn new() -> Self {
+        let (tx, rx) = mpsc::channel();
         Self {
             _phantom: PhantomData,
-            f: Some(f),
-            output: None,
+            tasks_to_spawn: Vec::new(),
+            tasks: Vec::new(),
+            tasks_done: 0,
+            tx,
+            rx,
         }
     }
 }
 
-impl<T, F> Future for Scope<T, F>
+impl<'scope, Ex> Future for ScopeGuard<'scope, Ex>
 where
-    F: FnOnce(&mut ScopedSpawner<T>) -> T,
-    T: Send,
+    Ex: Executor,
+    Ex::TaskHandle<()>: Unpin,
 {
-    type Output = T;
+    type Output = ();
 
     fn poll(
-        self: std::pin::Pin<&mut Self>,
-        _cx: &mut std::task::Context<'_>,
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut Context<'_>,
     ) -> std::task::Poll<Self::Output> {
-        // TODO: Give the `ScopedSpawnCapableFuture` a pointer to the `self` so it can take and
-        // call `self.f`. Now that we've been pinned, this should be safe.
-        if !IN_SUPPORTED_RUNTIME.get() {
-            panic!("Scope can only be polled from within a supported async runtime");
+        let this = self.as_mut().get_mut();
+        let n_tasks_to_spawn = this.tasks_to_spawn.len();
+        if n_tasks_to_spawn > 0 {
+            this.tasks.reserve(n_tasks_to_spawn);
+            for (i, task) in this.tasks_to_spawn.drain(..).enumerate() {
+                this.tasks.push(SubTask {
+                    state: TaskState {
+                        lock: AtomicBool::new(true),
+                        inner: UnsafeCell::new(InnerTaskState {
+                            future: Box::into_pin(task),
+                            scope_waker: cx.waker().clone(),
+                            waker: None,
+                            done: false,
+                        }),
+                    },
+                    runner_handle: None,
+                    wake: true,
+                });
+                let task = this.tasks.last_mut().unwrap();
+                task.runner_handle = Some(Ex::spawn(ScopedTaskRunner {
+                    state: (&mut task.state).into(),
+                    tx: this.tx.clone(),
+                    idx: i,
+                    done: false,
+                }));
+            }
+        } else {
+            while let Ok(idx) = this.rx.try_recv() {
+                this.tasks[idx].wake = true;
+            }
+            for task in this.tasks.iter_mut() {
+                if !task.wake {
+                    continue;
+                }
+                // If there is no waker here, the runner may have panicked
+                if let Some(waker) =
+                    unsafe { task.state.inner.get().as_mut().unwrap().waker.take() }
+                {
+                    waker.wake();
+                    task.wake = true;
+                    task.state.lock.store(true, Ordering::Release);
+                }
+            }
         }
-        WANTS_TO_SPAWN_SCOPED_TASKS.set(Some((
-            (&mut self.f as *mut Option<F>).cast(),
-            (&mut self.output as *mut Option<T>).cast(),
-            |f, output, inner_spawner| {
-                let f: &mut Option<F> = unsafe { &mut *f.cast() };
-                let output: &mut Option<T> = unsafe { &mut *output.cast() };
-                *output = Some((f.take().unwrap())(&mut ScopedSpawner::new(inner_spawner)));
-            },
-        )));
-        // TODO: figure out wakers...
-        Poll::Pending
-    }
-}
-
-pub struct ScopedSpawner<T> {
-    _phantom: PhantomData<T>,
-    inner: ScopedSpawnerInner,
-}
-impl<T> ScopedSpawner<T> {
-    fn new(inner: ScopedSpawnerInner) -> ScopedSpawner<T>
-    where
-        T: Send,
-    {
-        Self {
-            _phantom: PhantomData,
-            inner,
+        let mut waiting_for = this.tasks.iter().filter(|task| task.wake).count();
+        while waiting_for > 0 {
+            for task in this.tasks.iter_mut() {
+                if !task.wake {
+                    continue;
+                }
+                while task.state.lock.load(Ordering::Acquire) {
+                    // You spin me right round, baby, right round
+                }
+                task.wake = false;
+                waiting_for -= 1;
+                if unsafe { task.state.inner.get().as_ref().unwrap().done } {
+                    this.tasks_done += 1;
+                }
+            }
+        }
+        if this.tasks_done == this.tasks.len() {
+            Poll::Ready(())
+        } else {
+            Poll::Pending
         }
     }
-
-    pub fn spawn<F, U>(&mut self, fut: F) -> JoinHandle<U>
-    where
-        F: Future<Output = U> + Send,
-    {
-        self.inner.spawn()
-    }
 }
 
-struct ScopedSpawnerInner {
-    //
-}
-
-impl ScopedSpawnerInner {
-    pub fn spawn<T>(&mut self) -> JoinHandle<T> {
-        todo!()
-    }
-}
-
-pub struct JoinHandle<T> {
-    _phantom: PhantomData<T>,
-}
-
-impl<T> JoinHandle<T> {}
-
-// TODO: Figure out how nested scoped tasks work
-// TODO: There may be a way of relaxing the requirement where `ScopedSpawnCapableFuture` must be at
-// the bottom of the futures stack.
-
-/// A "guard" of sorts that always is the "top-level" future in a given stack of futures. It
-/// allows us to defer spawning scoped tasks until we know we're about to give back control to
-/// the underlying executor, and also lets us guard against running the inner stack of futures
-/// while scoped tasks are still running.
-pub struct ScopedSpawnCapableFuture<F> {
-    // TODO: Does this need to be wrapped in an UnsafeCell?
-    inner: F,
-    subtasks_are_running: bool,
-    // We rely on being pinned by the executor until we're either dropped, leaked, or polled to
-    // completion.
-    _phantom: PhantomPinned,
-}
-
-impl<F> ScopedSpawnCapableFuture<F>
+impl<'scope, Ex> Drop for ScopeGuard<'scope, Ex>
 where
-    F: Future,
+    Ex: Executor,
 {
-    /// # Safety
-    ///
-    /// The caller is responsible for ensuring that this future isn't polled by anything other than
-    /// a supported executor, with no intermediate futures.
-    pub unsafe fn new(future: F) -> Self {
-        Self {
-            inner: future,
-            subtasks_are_running: false,
-            _phantom: PhantomPinned,
-        }
-    }
-
-    fn project_inner(self: Pin<&mut Self>) -> Pin<&mut F> {
-        unsafe { self.map_unchecked_mut(|s| &mut s.inner) }
-    }
-}
-
-thread_local! {
-    static IN_SUPPORTED_RUNTIME: Cell<bool> = Cell::new(false);
-    static WANTS_TO_SPAWN_SCOPED_TASKS: Cell<Option<(*mut (), *mut (), fn(*mut (), *mut (), ScopedSpawnerInner))>> = Cell::new(None);
-}
-
-impl<F> Future for ScopedSpawnCapableFuture<F>
-where
-    F: Future,
-{
-    type Output = F::Output;
-
-    fn poll(
-        self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Self::Output> {
-        if self.subtasks_are_running {
-            // TODO: register a waker
-            return Poll::Pending;
-        }
-
-        IN_SUPPORTED_RUNTIME.set(true);
-        let ret = self.project_inner().poll(cx);
-        IN_SUPPORTED_RUNTIME.set(false);
-        if let Some((f, output, callback)) = WANTS_TO_SPAWN_SCOPED_TASKS.get() {
-            let inner_spawner = ();
-            callback(f, output, inner_spawner);
-            self.subtasks_are_running = true;
-        }
-        ret
-    }
-}
-
-impl<F> Drop for ScopedSpawnCapableFuture<F> {
     fn drop(&mut self) {
-        inner_drop(unsafe { Pin::new_unchecked(self) });
-        fn inner_drop<F>(this: Pin<&mut ScopedSpawnCapableFuture<F>>) {
-            // TODO: Make sure all scoped tasks are either cancelled or complete before returning from
-            // this function. Leaking this future should however still be safe. Probably involves
-            // looping on some shared atomic counter for the number of live scoped sub-tasks.
-            while this.subtasks_are_running {}
+        for task in self.tasks.iter() {
+            if let Some(runner_handle) = task.runner_handle.as_ref() {
+                runner_handle.abort();
+            }
         }
     }
+}
+
+pub struct Scope<'scope, 'env: 'scope, Ex>
+where
+    Ex: Executor,
+{
+    // *const is so the struct is !Send, since spawning subtasks within a subtask from the Scope
+    // isn't something I want to tackle right now. Using a different inner Scope should be fine.
+    _phantom: PhantomData<*const &'scope &'env ()>,
+    guard: Mutex<ScopeGuard<'scope, Ex>>,
+}
+
+impl<'scope, 'env, Ex> Scope<'scope, 'env, Ex>
+where
+    Ex: Executor,
+{
+    fn new() -> Scope<'scope, 'env, Ex> {
+        Self {
+            _phantom: PhantomData,
+            guard: Mutex::new(ScopeGuard::new()),
+        }
+    }
+
+    pub fn spawn<F, T>(&self, fut: F)
+    where
+        F: Future<Output = T> + Send + 'scope,
+        T: Send + 'scope,
+    {
+        let fut = Box::new(async {
+            fut.await;
+            // FIXME: Do something with the output of the future instead of throwing it away
+        }) as Box<dyn Future<Output = ()>>;
+        // SAFETY: The 'scope lifetime is enforced by ... this entire module, really.
+        let fut = unsafe {
+            mem::transmute::<
+                Box<dyn Future<Output = ()> + 'scope>,
+                Box<dyn Future<Output = ()> + 'static>,
+            >(fut)
+        };
+        self.guard.lock().unwrap().tasks_to_spawn.push(fut);
+    }
+}
+
+struct SubTask<H> {
+    state: TaskState,
+    runner_handle: Option<H>,
+    wake: bool,
+}
+
+unsafe impl<H> Send for SubTask<H> {}
+
+struct TaskState {
+    // Since the executor may poll the ScopedTaskRunner as it pleases, we have to guard against the
+    // runtime polling us at the wrong time. The "lock" is `true` when the ScopedTaskRunner is
+    // allowed to access the guarded state, and `false` when the ScopeGuard is allowed to access
+    // the state.
+    lock: AtomicBool,
+    inner: UnsafeCell<InnerTaskState>,
+}
+
+struct InnerTaskState {
+    future: Pin<Box<dyn Future<Output = ()>>>,
+    scope_waker: Waker,
+    waker: Option<Waker>,
+    done: bool,
+}
+
+/// The future spawned directly onto the runtime.
+struct ScopedTaskRunner {
+    // We're only allowed to access this when `task_fence == true`
+    state: NonNull<TaskState>,
+    tx: mpsc::Sender<usize>,
+    idx: usize,
+    done: bool,
+}
+
+unsafe impl Send for ScopedTaskRunner {}
+
+impl Future for ScopedTaskRunner {
+    type Output = ();
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if self.done {
+            panic!("Polled ScopedTaskRuner after it completed");
+        }
+        if unsafe { self.state.as_ref().lock.load(Ordering::Acquire) } {
+            let ret = {
+                let this = self.as_mut().get_mut();
+                let state = unsafe { this.state.as_ref().inner.get().as_mut().unwrap_unchecked() };
+                let future = state.future.as_mut();
+                // FIXME: Panic propogation?
+                // FIXME: Release the lock on panic?
+                let waker = scoped_task_waker(state.scope_waker.clone(), this.tx.clone(), this.idx);
+                let ret = future.poll(&mut Context::from_waker(&waker));
+                state.waker = Some(cx.waker().clone());
+                state.done = ret.is_ready();
+                this.done = ret.is_ready();
+                ret
+            };
+            unsafe { self.state.as_ref().lock.store(false, Ordering::Release) };
+            ret
+        } else {
+            Poll::Pending
+        }
+    }
+}
+
+fn scoped_task_waker(scope_waker: Waker, tx: mpsc::Sender<usize>, idx: usize) -> Waker {
+    struct ScopedTaskWakerData {
+        scope_waker: Waker,
+        tx: mpsc::Sender<usize>,
+        idx: usize,
+    }
+    const VTABLE: RawWakerVTable = RawWakerVTable::new(
+        |data| {
+            let data = unsafe { Arc::from_raw(data.cast::<ScopedTaskWakerData>()) };
+            let cloned = RawWaker::new(Arc::into_raw(Arc::clone(&data)).cast(), &VTABLE);
+            let _ = Arc::into_raw(data);
+            cloned
+        },
+        |data| {
+            let data = unsafe { Arc::from_raw(data.cast::<ScopedTaskWakerData>()) };
+            data.scope_waker.wake_by_ref();
+            let _ = data.tx.send(data.idx);
+        },
+        |data| {
+            let data = unsafe { Arc::from_raw(data.cast::<ScopedTaskWakerData>()) };
+            let _ = data.tx.send(data.idx);
+            data.scope_waker.wake_by_ref();
+            let _ = Arc::into_raw(data);
+        },
+        |data| {
+            let _ = unsafe { Arc::from_raw(data.cast::<ScopedTaskWakerData>()) };
+        },
+    );
+    let data: Arc<ScopedTaskWakerData> = Arc::new(ScopedTaskWakerData {
+        scope_waker,
+        tx,
+        idx,
+    });
+    let raw = RawWaker::new(Arc::into_raw(data).cast(), &VTABLE);
+    unsafe { Waker::from_raw(raw) }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::{mem, pin::pin};
-
     use super::*;
 
-    #[tokio::test(flavor = "multi_thread")]
-    async fn test_evil_fanout() {
-        async fn evil_fanout<T: Send + Sync>(data: &mut Vec<T>) {
-            {
-                let scope = scope(|s| {
-                    for n in data.iter() {
-                        s.spawn(async {
-                            let n = n;
-                            tokio::task::yield_now().await;
-                            mem::drop(n);
-                        });
-                    }
-                });
-                // The `Scope` will register its callback with the `ScopedSpawnCapableFuture` once.
-                let mut scope = Box::pin(scope);
-                futures::poll!(&mut scope);
-                // This will invalidate `scope`'s borrow of `data`. Since the callback given to
-                // `scope` hasn't been run yet, nothing bad can happen.
-                mem::forget(scope);
+    struct TokioExecutor {}
+    impl Executor for TokioExecutor {
+        type TaskHandle<T> = tokio::task::JoinHandle<T>;
 
-                let foo = data;
-                // Yielding back to the `ScopedSpawnCapableFuture` must not spawn the scoped tasks.
-                // However, we have no way of guaranteeing this :(
-                tokio::task::yield_now().await;
-                mem::drop(foo);
-            }
+        fn spawn<F, T>(fut: F) -> Self::TaskHandle<T>
+        where
+            F: Future<Output = T> + Send + 'static,
+            T: Send + 'static,
+        {
+            tokio::task::spawn(fut)
         }
-        let data = vec![1, 3, 5, 7];
-        tokio::spawn(evil_fanout(&mut data));
+    }
+    impl<T> TaskHandle for tokio::task::JoinHandle<T> {
+        fn abort(&self) {
+            tokio::task::JoinHandle::<T>::abort(self)
+        }
     }
 
-    #[tokio::test(flavor = "multi_thread")]
-    async fn test_evil_fanout_stack_pin() {
-        async fn evil_fanout<T: Send + Sync>(data: &mut Vec<T>) {
+    #[test]
+    fn test_evil_fanout() {
+        async fn evil_fanout(data: &mut Vec<i32>) {
             {
-                let scope = scope(|s| {
-                    for n in data.iter() {
-                        s.spawn(async {
-                            let n = n;
-                            tokio::task::yield_now().await;
-                            mem::drop(n);
-                        });
-                    }
+                let my_scope = scope::<TokioExecutor, _>(|s| {
+                    s.spawn(async {
+                        tokio::task::yield_now().await;
+                        data.push(1);
+                    });
+                    s.spawn(async {
+                        tokio::task::yield_now().await;
+                    });
                 });
-                // The `Scope` will register its callback with the `ScopedSpawnCapableFuture` once.
-                let mut scope = pin!(scope);
-                futures::poll!(&mut scope);
-                // This will invalidate `scope`'s borrow of `data`. Since the callback given to
-                // `scope` hasn't been run yet, nothing bad can happen.
+                // Once awaited, the scope will spawn all but the first task onto the runtime, and
+                // then poll the first task itself. `Scope::poll` will only yield once its child
+                // tasks have all yielded.
+                let mut scope = Box::pin(my_scope);
+                let _ = futures::poll!(&mut scope);
+
+                // E501: Cannot borrow data as mutable because previous closure
+                // requires unique access
+                // data.push(0);
+
+                // This will invalidate `scope`'s borrow of `data`. Since the child tasks may only
+                // run (and thus access the futures passed to `s.spawn()`) when we poll `scope`, no
+                // use-after-free is possible.
                 mem::forget(scope);
 
-                let foo = data;
-                // Yielding back to the `ScopedSpawnCapableFuture` must not spawn the scoped tasks.
-                tokio::task::yield_now().await;
-                mem::drop(foo);
+                data.push(2);
             }
-        };
-        let data = vec![1, 3, 5, 7];
-        tokio::spawn(evil_fanout(&mut data));
+        }
+
+        let runtime = tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+        let mut data = vec![1, 3, 5, 7];
+        runtime.block_on(evil_fanout(&mut data));
+        assert_eq!(data, vec![1, 3, 5, 7, 2]);
     }
 }
